58e1ff4e3633192149da8c0e16437deaa023adf4
Jaudielhf <jaudielhernandez23@gmail.com>
2024-06-18 23:40:42 -0600

    refactor(api key): cambio
    Api key de Firebase para la conexion de esta misma

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,13 @@
 export const environment = {
   production: false,
    firebaseConfig : {
-    apiKey: "AIzaSyCacuH6ws2GH63-QOZtVzktssnih2IkQSo",
-    authDomain: "appmiches.firebaseapp.com",
-    projectId: "appmiches",
-    storageBucket: "appmiches.appspot.com",
-    messagingSenderId: "599747574853",
-    appId: "1:599747574853:web:c7b2a98161a2a52ec3372c",
-    measurementId: "G-QYWRN8RDB0"
+    apiKey: "AIzaSyAeFd4o37SO5dwDh1TQZVl2OQW513pD1YM",
+    authDomain: "appcerverfriowagner44.firebaseapp.com",
+    projectId: "appcerverfriowagner44",
+    storageBucket: "appcerverfriowagner44.appspot.com",
+    messagingSenderId: "886130637531",
+    appId: "1:886130637531:web:cc8b8a93be393d21892afe",
+    measurementId: "G-3GSFXJQGY0"
   }
 };
 
